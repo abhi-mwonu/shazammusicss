551952be992b0ef630e641e1c2e2736026be04c0
Pʀᴇᴅᴀᴛᴏʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-02-16 21:01:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
-class Config:
-    API_ID = "9049867"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "972c0f2e94973b5ffa9a89cb34e5671b" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "5149412762:AAF1w1IwWC65J5_7Lj5yCs-6iRBhhaP2qvM" 
+import os
+
+class Config(object):
+    API_ID = int(os.environ.get("API_ID", 12345))
+    API_HASH = os.environ.get("API_HASH", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+   
