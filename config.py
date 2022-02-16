160f640f5d6e9bdeb77291fbf1a91fd151ea3db7
Pʀᴇᴅᴀᴛᴏʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-02-16 20:02:30 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    API_ID = "9049867"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "972c0f2e94973b5ffa9a89cb34e5671b" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "5149412762:AAF1w1IwWC65J5_7Lj5yCs-6iRBhhaP2qvM" 
