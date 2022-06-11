17ff4e4978392260eb1c0ce3c22708ee67fe3314
ツAʙʜɪsʜᴇᴋ Kᴜᴍᴀʀ <77227205+PredatorHackerzZ@users.noreply.github.com>
2022-06-11 09:19:28 +0530

    Update and rename bot - 2022-06-11T091446.940.py to bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 """
     Shazam Telegram Bot
-    Copyright (C) 2021 @ImJanindu
+    Copyright (C) 2021 @PredatorHackerzZ
     
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
